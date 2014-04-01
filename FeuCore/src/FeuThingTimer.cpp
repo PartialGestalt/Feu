0231277a403a4dbc9f9b8186c34028954c100da0
Andrew Kephart <akephart@alereon.com>
2014-03-31 22:13:43 -0500

    add Thing class for timer elements

---
+++ FeuCore/src/FeuThingTimer.cpp
@@ -0,0 +1,17 @@
+/*
+ * FeuThingTimer.cpp
+ *
+ *  Created on: Mar 31, 2014
+ *      Author: akephart
+ */
+
+#include "FeuThingTimer.h"
+
+FeuThingTimer::FeuThingTimer(Feu *feu, TiXmlElement *ele, FeuThing *parent) : FeuThing(feu,ele,parent) {
+    // CLEAN: TODO: ???
+}
+
+
+FeuThingTimer::~FeuThingTimer() {
+}
+
---
+++ FeuCore/src/FeuThingTimer.h
@@ -0,0 +1,19 @@
+/*
+ * FeuThingTimer.h
+ *
+ *  Created on: Mar 31, 2014
+ *      Author: akephart
+ */
+
+#ifndef FEUTHINGTIMER_H_
+#define FEUTHINGTIMER_H_
+
+#include "FeuThing.h"
+
+class FeuThingTimer: public FeuThing {
+public:
+	FeuThingTimer(Feu *feu, TiXmlElement *ele, FeuThing *parent);
+	virtual ~FeuThingTimer();
+};
+
+#endif /* FEUTHINGTIMER_H_ */
